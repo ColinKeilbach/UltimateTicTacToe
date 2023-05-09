83df07204139e79e0770349dbd88a0589327948a
Colin Keilbach <CJKeilbach19@gmail.com>
2023-05-08 20:00:47 -0400

    Fixed Hard AI not working in browser

--- Assets/Scripts/Player/MiniMaxPlayer.cs
+++ Assets/Scripts/Player/MiniMaxPlayer.cs
@@ -5,13 +5,27 @@ using UnityEngine;
 
 public class MiniMaxPlayer : Player
 {
+# if PLATFORM_WEBGL
+    private readonly IScorer scorer = new RowScoring();
+    public override void RequestMove(Board board, ReturnMove returnMove)
+    {
+        Thinking = true;
+        MiniMax.Evaluate(board, scorer, Tile.ValueToInt(board.GetXToMove() ? Tile.Value.X : Tile.Value.O), 3, (move, score) =>
+        {
+            Thinking = false;
+            returnMove.Invoke(move);
+        });
+    }
+#else
     private readonly ThreadedMiniMax tmm = new();
     public override void RequestMove(Board board, ReturnMove returnMove)
     {
+        Thinking = true;
         tmm.Evaluate(board, Tile.ValueToInt(board.GetXToMove() ? Tile.Value.X : Tile.Value.O), 5, (move) =>
         {
             Thinking = false;
             returnMove.Invoke(move);
         });
     }
+#endif
 }
